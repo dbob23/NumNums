04f89011f4e51e89f2368230837c5c28d137d9b1
Dennis Segall <dennis.segall@sbcglobal.net>
2019-07-30 22:40:24 -0500

    initial commit

---
+++ src/main/java/com/NumNums/NumNumsApplication.java
@@ -0,0 +1,13 @@
+package com.NumNums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumNumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumNumsApplication.class, args);
+	}
+
+}
