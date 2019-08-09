31f263f6d5649265a17af373b1497e84bad66501
Dennis Segall <dennis.segall@sbcglobal.net>
2019-08-09 14:01:53 -0500

    in progress: zip-code

--- src/main/java/com/NumNums/controlllers/HomeController.java
+++ src/main/java/com/NumNums/controlllers/HomeController.java
@@ -1,22 +1,40 @@
 package com.NumNums.controlllers;
 
 
+import com.NumNums.models.SearchDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-//Woo hoo! Good times with Git!
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping("NumNums")
 public class HomeController {
 
-    @RequestMapping(value="")
+    @RequestMapping(value="", method = RequestMethod.GET)
     public String index(Model model){
 
         model.addAttribute("title", "NumNums!");
         return "home/index";
     }
 
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public String processZipCode(@ModelAttribute @Valid SearchDetails aSearch, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "NumNums!");
+            return "home/index";
+        }
 
-}
+
+        model.addAttribute("title", "NumNums!");
+        model.addAttribute( "SearchDetails", aSearch);
+
+//        test
+
+        return "home/test";
+    }
+
+    }
