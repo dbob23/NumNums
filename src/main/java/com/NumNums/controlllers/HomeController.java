dd993fdb6124f511d711d9c26d19e802e5e924e4
Dennis Segall <dennis.segall@sbcglobal.net>
2019-08-11 20:27:52 -0500

    in progress: zip-code with regex

--- src/main/java/com/NumNums/controlllers/HomeController.java
+++ src/main/java/com/NumNums/controlllers/HomeController.java
@@ -4,10 +4,13 @@ package com.NumNums.controlllers;
 import com.NumNums.models.SearchDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 
 @Controller
@@ -22,15 +25,17 @@ public class HomeController {
     }
 
     @RequestMapping(value="", method = RequestMethod.POST)
-    public String processZipCode(@ModelAttribute @Valid SearchDetails aSearch, Errors errors, Model model) {
+    public String processZipCode(@ModelAttribute @Valid  SearchDetails aSearch, Model model, Errors errors) {
+
         if (errors.hasErrors()) {
             model.addAttribute("title", "NumNums!");
+            model.addAttribute( "aSearch", new SearchDetails());
             return "home/index";
         }
 
 
         model.addAttribute("title", "NumNums!");
-        model.addAttribute( "SearchDetails", aSearch);
+        model.addAttribute( "aSearch", new SearchDetails());
 
 //        test
 
