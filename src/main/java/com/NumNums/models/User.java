5a10fa590fb1ee4497cdfe3c315531aa2306dff4
Dennis Segall <dennis.segall@sbcglobal.net>
2019-08-15 12:10:15 -0500

    in progress: user signup. Spring Security implemented.

--- src/main/java/com/NumNums/models/User.java
+++ src/main/java/com/NumNums/models/User.java
@@ -1,36 +1,59 @@
 package com.NumNums.models;
 
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.validation.constraints.NotNull;
-
+import org.hibernate.validator.constraints.Length;
+//import javax.management.relation.Role;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
+@Table(name = "user")
 public class User {
 
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
-    Integer id;
+    @Column(name = "user_id")
+    private int id;
 
     @Column(unique = true)
-    @NotNull
-    String username;
+    @NotEmpty(message = "Please provide your name.")
+    private String username;
 
-    @Column
-    @NotNull
-    String password;
+    @Column(name = "email")
+    @Email(message = "Please provide a valid email.")
+    @NotEmpty(message = "Please provide an email.")
+    private String email;
+
+    @Column(name = "password")
+    @Length(min = 5, message = "Your password must have at least 5 characters.")
+    @NotEmpty(message = "Please provide your password.")
+    private String password;
+
+
+    @Column(name = "active")
+    private int active;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name= "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set <Role> roles;
+
 
     public User() {
     }
 
-    public User(Integer id, String username, String password) {
+    public User(Integer id, String username, String password, String email, Set roles) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.active = 0;
+        this.roles = roles;
+
     }
+
 
     public Integer getId() {
         return id;
@@ -56,8 +79,30 @@ public class User {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
 
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(HashSet<com.NumNums.models.Role> roles) {
+        this.roles = roles;
+    }
 }
 
 
