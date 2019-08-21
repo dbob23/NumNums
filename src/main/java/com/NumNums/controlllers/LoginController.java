d006e026301e7e78adc4fdfda9735f1d8cbe4240
Dennis Segall <dennis.segall@sbcglobal.net>
2019-08-21 09:26:37 -0500

    changes made to loginComtroller

--- src/main/java/com/NumNums/controlllers/LoginController.java
+++ src/main/java/com/NumNums/controlllers/LoginController.java
@@ -21,50 +21,61 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value= "NumNums/login", method = RequestMethod.GET)
-    public String displayLoginForm(@ModelAttribute("aLogInUserObject") LogInUserObject aLogInUserObject, Model model) {
-        model.addAttribute("aLoginUserObject", new LogInUserObject());
-        model.addAttribute("title", "Log In");
-        return "login/login";
-    }
-
-    @RequestMapping(value = "NumNums/login", method = RequestMethod.POST)
-    public String processLoginForm(@ModelAttribute("aLogInUserObject") LogInUserObject aLogInUserObject, Model model) {
-
-        model.addAttribute("aLogInUserObject", new LogInUserObject());
-        User aUser = userService.findUserByEmail(aLogInUserObject.getEmail());
-
-        if (aUser == null){
-            model.addAttribute("message", "You have entered an invalid email or password.");
-            return "login/login";
-        }
-        if ( !aUser.getPassword().equals(aLogInUserObject.getPassword())){
-            model.addAttribute("message", "You have entered an invalid email or password.");
-            return "login/login";
-        }
-
-        aUser.setActive(2);
-        model.addAttribute("title", "Welcome");
-        model.addAttribute("user", aUser);
-        model.addAttribute("message", "You are successfully logged in!");
-        return "home/display";
-       }
+//    @RequestMapping(value= "NumNums/login", method = RequestMethod.GET)
+//    public String displayLoginForm(@ModelAttribute("aLogInUserObject") LogInUserObject aLogInUserObject, Model model) {
+//        model.addAttribute("aLoginUserObject", new LogInUserObject());
+//        model.addAttribute("title", "Log In");
+//        return "login/login";
+//    }
+//
+//    @RequestMapping(value = "NumNums/login", method = RequestMethod.POST)
+//    public String processLoginForm(@ModelAttribute("aLogInUserObject") LogInUserObject aLogInUserObject, Model model) {
+//
+//        model.addAttribute("aLogInUserObject", new LogInUserObject());
+//        User aUser = userService.findUserByEmail(aLogInUserObject.getEmail());
+//
+//        if (aUser == null){
+//            model.addAttribute("message", "You have entered an invalid email or password.");
+//            return "login/login";
+//        }
+//        if ( !aUser.getPassword().equals(aLogInUserObject.getPassword())){
+//            model.addAttribute("message", "You have entered an invalid email or password.");
+//            return "login/login";
+//        }
+//
+//        aUser.setActive(2);
+//        model.addAttribute("title", "Welcome");
+//        model.addAttribute("user", aUser);
+//        model.addAttribute("message", "You are successfully logged in!");
+//        return "home/display";
+//       }
 
 //       tutorial controller
 
-//    @RequestMapping(value="NumNums/login", method = RequestMethod.GET)
-//    public ModelAndView login(){
-//        ModelAndView modelAndView = new ModelAndView();
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        User user = userService.findUserByEmail(auth.getName());
-//        modelAndView.addObject("userName", "Welcome " + user.getUsername() + "  (" + user.getEmail() + ")");
-//        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-//        modelAndView.setViewName("admin/home");
-//        return modelAndView;
-//    }
+    @RequestMapping(value="NumNums/login", method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        if (user != null) {
 
 
-}
+        modelAndView.addObject("userName", "Welcome " + user.getUsername() + "  (" + user.getEmail() + ")");
+        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("admin/home");
+        }
+        else {
+
+        modelAndView.addObject("aLogInUserObject", new LogInUserObject());
+        modelAndView.addObject("title", "Log In");
+        modelAndView.setViewName("login/login");
+        }
+        return modelAndView;
+    }
+    }
+
+
+
 
 
 
