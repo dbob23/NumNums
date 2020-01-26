d0e1115a7008e5a6277f51f94eb6139622203cdc
Dennis Segall <dennis.segall@sbcglobal.net>
2020-01-25 22:32:54 -0600

    map and marker are functioning, list view works with hard coded value, but locate restaurant function is broken.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,4 @@
+/*
 var map;
 
 function initMap() {
@@ -8,3 +9,16 @@ function initMap() {
     };
     map = new google.maps.Map(document.getElementById('map'), options);
 }
+*/
+
+ var map;
+
+    function initMap() {
+
+    var options = {
+    center: {lat: 38.6270, lng: -90.1994},
+    zoom: 8
+    };
+    map = new google.maps.Map(document.getElementById('map'), options);
+    }
+
