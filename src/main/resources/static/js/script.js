009255d439f19f63e62431016860617d87a79531
Dennis Segall <dennis.segall@sbcglobal.net>
2020-01-30 13:51:46 -0600

    changes made to nav bar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,4 +7,10 @@ function initMap() {
         zoom: 8
     };
     map = new google.maps.Map(document.getElementById('map'), options);
+
+    var marker = new google.maps.Marker({
+                position:{lat: 38.5950619, lng: -90.2291565},
+                map:map
+               });
+            }
 }
