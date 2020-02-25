a6e468adf6ee96e99655ea2fcbb0ca250332617b
Dennis Segall <dennis.segall@sbcglobal.net>
2020-02-25 10:57:39 -0600

    All navbars refactored to match logged in/logged out status of user

--- src/main/java/com/NumNums/controlllers/AboutController.java
+++ src/main/java/com/NumNums/controlllers/AboutController.java
@@ -1,5 +1,10 @@
 package com.NumNums.controlllers;
 
+import com.NumNums.models.User;
+import com.NumNums.models.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AboutController {
 
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value="NumNums/about", method = RequestMethod.GET)
     public String displayAboutPage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+
+        if (user!=null) {
+            model.addAttribute("home", "/NumNums/login"  );
+
+        }
+
         model.addAttribute("title", "About NumNums");
         return "about/about";
     }
